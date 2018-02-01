db0d9698d832d1469f628bd4ec0573fa5d882984
sibolatte <35409146+sibolatte@users.noreply.github.com>
2018-02-01 22:25:24 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
-
+##Let's create makeCacheMatrix and CacheSolve
 ## Write a short comment describing this function
+# makeCacheMatrix creates a list containing a function to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -17,7 +20,7 @@ makeCacheMatrix <- function(x = matrix()) {
        getinv = getinv)
 }
 
-## Write a short comment describing this function
+##and cacheSolve function assumes that the matrix is always invertible
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -31,3 +34,17 @@ cacheSolve <- function(x, ...) {
   x$setinv(inv)
   inv
 }
+
+## Sample Example
+## x = rbind(c(1,1/2), c(-1/2, 1))
+## s = makeCacheMatrix(x)
+## s$get()
+##       [,1]  [,2]
+## [1,]  1.00 0.50
+## [2,] -0.50  1.00
+
+## No cache in the first run
+## cacheSolve(s)
+## [,1] [,2]
+## [1,]  0.8 -0.4
+## [2,]  0.4  0.8
